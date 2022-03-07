8026f5eacaee3768bef13c663c4f33c42338aab7
M Sahithi <msahithi254@gmail.com>
2022-03-07 12:11:50 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstractClass
+{
+    internal class Program
+    {
+        public abstract class Pens
+        {
+            public abstract void display();
+        }
+
+        public class Blue : Pens
+        {
+            public override void display()
+            {
+                Console.WriteLine("Blue");
+            }
+        }
+
+        public class Black : Pens
+        {
+            public override void display()
+            {
+                Console.WriteLine("Black");
+            }
+        }
+
+        public class Red : Pens
+        {
+            public override void display()
+            {
+                Console.WriteLine("Red");
+            }
+        }
+
+            public class MyClass
+        {
+            public static void Main()
+            {
+                Pens p;
+                p = new Blue();
+                p.display();
+                p = new Black();
+                p.display();
+                p = new Red();
+                p.display();
+                Console.ReadLine();
+            }
+        }
+    }
+}
